603299b021fec8491721a187671ec2caf8f7af10
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2024-08-14 22:30:41 +0100

    Tenth Commit

--- capstone_terraform/pre-apply.sh
+++ capstone_terraform/pre-apply.sh
@@ -12,4 +12,4 @@ az group create --name $RESOURCE_GROUP_NAME --location $REGION
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
 
 # Create blob container
-az storage container create --name CONTAINER_NAME --account-name STORAGE_ACCOUNT_NAME
+az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
