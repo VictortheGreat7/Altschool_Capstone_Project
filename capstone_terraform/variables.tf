c6a698419be0bcd2565e507e28d86d98bb5e5d40
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2024-08-15 04:09:57 +0100

    Thirtieth Commit

--- capstone_terraform/variables.tf
+++ capstone_terraform/variables.tf
@@ -21,22 +21,22 @@ variable "allowed_source_addresses" {
   type        = list(string)
 }
 
-variable "client_id" {
-  description = "The client id of the service principal"
-  type        = string
-}
+# variable "client_id" {
+#   description = "The client id of the service principal"
+#   type        = string
+# }
 
-variable "tenant_id" {
-  description = "The tenant id of the service principal"
-  type        = string
-}
+# variable "client_secret" {
+#   description = "The client secret of the service principal"
+#   type        = string
+# }
 
-variable "client_secret" {
-  description = "The client secret of the service principal"
-  type        = string
-}
+# variable "tenant_id" {
+#   description = "The tenant id of the service principal"
+#   type        = string
+# }
 
-variable "subscription_id" {
-  description = "The subscription id"
-  type        = string
-}
+# variable "subscription_id" {
+#   description = "The subscription id"
+#   type        = string
+# } 
